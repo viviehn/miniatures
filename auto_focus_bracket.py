08757c4cabc55976291bf03b2d2152727abe8ad0
Vivien Nguyen <vivienn@princeton.edu>
2025-10-09 12:50:54 -0400

    Update auto focus bracket script

--- auto_focus_bracket.py
+++ auto_focus_bracket.py
@@ -23,45 +23,63 @@ WAIT_TO_SHOOT=2
 NEAR_STR='near1'
 FAR_STR='far1'
 
-WIDE_AV='f4.0'
-MED_AV='f8.0'
-NARROW_AV='f16'
+WIDE_AV='f2.8'
+MED_AV='f5.6'
+NARROW_AV='f8.0'
 
 def put_av(camera, av):
-    time.sleep(WAIT_TO_FOCUS)
     ret = camera._put(path='/ccapi/ver100/shooting/settings/av',
                  json={"value": av})
+    while ret.status_code != 200:
+        time.sleep(.25)
+        ret = camera._put(path='/ccapi/ver100/shooting/settings/av',
+                     json={"value": av})
+    return ret
 
 def configure_focus_bracketing(camera, step_size=1, num_steps=49):
-    time.sleep(WAIT_TO_FOCUS)
     focus_api_path = '/ccapi/ver100/shooting/settings/focusbracketing'
 
-    camera._put(path=f'{focus_api_path}',
+    ret = camera._put(path=f'{focus_api_path}',
                  json={"value": "enable"})
+    while ret.status_code != 200:
+        time.sleep(.25)
+        ret = camera._put(path=f'{focus_api_path}',
+                     json={"value": "enable"})
 
-    camera._put(path=f'{focus_api_path}/focusincrement',
+    ret = camera._put(path=f'{focus_api_path}/focusincrement',
+             json={"value": step_size})
+    while ret.status_code != 200:
+        time.sleep(.25)
+        ret = camera._put(path=f'{focus_api_path}/focusincrement',
                  json={"value": step_size})
 
-    camera._put(path=f'{focus_api_path}/numberofshots',
+    time.sleep(.25)
+    ret = camera._put(path=f'{focus_api_path}/numberofshots',
                  json={"value": num_steps})
+    while ret.status_code != 200:
+        time.sleep(.25)
+        ret = camera._put(path=f'{focus_api_path}/numberofshots',
+                     json={"value": num_steps})
 
 def take_photo(camera):
-    time.sleep(WAIT_TO_SHOOT)
-    camera.shoot(af=False)
-    time.sleep(WAIT_TO_SHOOT)
+    time.sleep(1)
+    ret = camera.shoot(af=False)
 
 def post_focus_drive(camera, step):
-    time.sleep(WAIT_TO_FOCUS)
     ret = camera._post(path='/ccapi/ver100/shooting/control/drivefocus',
                  json={"value": step})
+    while ret.status_code != 200:
+        time.sleep(.25)
+        ret = camera._post(path='/ccapi/ver100/shooting/control/drivefocus',
+                     json={"value": step})
+    return ret
 
 def reset_focus(camera, num_steps):
     for _ in range(num_steps):
-        time.sleep(.2)
-        post_focus_drive(camera, 'near3')
-        print(_)
+        ret = post_focus_drive(camera, 'near3')
 
 
+# 10.09.2025 todo: run with larger step size and larger rot_degree?
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_shoot_steps', type=int, default=9)
@@ -88,32 +106,45 @@ def main():
 
     camera = CCAPI(debug=DEBUG, ip=CANON_IP)
 
+    start_position = 0
+
     for rot_iter in tqdm.tqdm(range(int(360/rot_degree))):
-        #new_position = start_position + rot_iter*rot_degree/(RESCALE_FACTOR)
-        time.sleep(WAIT_TO_TURN)
-        #stepper.setTargetPosition(new_position)
+        print(rot_iter)
+        new_position = start_position + rot_iter*rot_degree/(RESCALE_FACTOR)
 
         num_wide_steps = args.num_shoot_steps
-        num_med_steps = int(args.num_shoot_steps // 5) + 1
-        num_narrow_steps = int(args.num_shoot_steps // 9) + 1
+        num_med_steps = int(args.num_shoot_steps // 3) + 1
+        #num_narrow_steps = int(args.num_shoot_steps // 9) + 1
+        num_narrow_steps = 9
 
-        reset_focus(camera, num_narrow_steps)
+        reset_focus(camera, 10)
         put_av(camera, WIDE_AV)
-        configure_focus_bracketing(camera, step_size=1, num_steps=num_wide_steps)
+        print(WIDE_AV)
+
+        #stepper.setTargetPosition(new_position)
+        print('turning table')
+        time.sleep(WAIT_TO_TURN)
+
+        configure_focus_bracketing(camera, step_size=3, num_steps=num_wide_steps)
         take_photo(camera)
 
-        reset_focus(camera, num_narrow_steps)
+        reset_focus(camera, 10)
         put_av(camera, MED_AV)
-        configure_focus_bracketing(camera, step_size=5, num_steps=num_med_steps)
+        print(MED_AV)
+        configure_focus_bracketing(camera, step_size=1, num_steps=num_med_steps)
         take_photo(camera)
 
-        reset_focus(camera, num_narrow_steps)
+        reset_focus(camera, 10)
         put_av(camera, NARROW_AV)
-        configure_focus_bracketing(camera, step_size=9, num_steps=num_narrow_steps)
+        print(NARROW_AV)
+        configure_focus_bracketing(camera, step_size=1, num_steps=num_narrow_steps)
         take_photo(camera)
+        time.sleep(WAIT_TO_TURN)
 
         if args.single:
             break
+        if rot_iter > 0 and rot_iter%6 == 0:
+            time.sleep(3)
 
 
 if  __name__ == "__main__":
