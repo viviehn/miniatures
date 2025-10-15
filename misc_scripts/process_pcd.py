946699e7553cc45ab592912e742bf26e0210503c
vivien <vivienn@princeton.edu>
2025-10-15 19:51:55 -0400

    Add results dir

--- misc_scripts/process_pcd.py
+++ misc_scripts/process_pcd.py
@@ -1,14 +1,7 @@
 import open3d as o3d
 import numpy as np
+import argparse
 
-#data = 'pyramidcube2'
-pcd_name = 'macro_nerf'
-
-#pcd = o3d.io.read_point_cloud(f'colmap_exports/0421/{data}_fused.ply')
-#pcd = o3d.io.read_point_cloud(f'colmap_exports/0421/0421_recons/{data}/phone_24mm/colmap_00-1/dense/fused.ply')
-pcd = o3d.io.read_point_cloud(f'{pcd_name}.ply')
-points = np.array(pcd.points)
-colors = np.array(pcd.colors)
 
 
 def rgb2hsv(rgb):
@@ -35,6 +28,71 @@ def rgb2hsv(rgb):
 
     return hsv
 
+def filter_hsv(hsv_colors):
+    return
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input_file', type=str)
+    parser.add_argument('--output_file', type=str)
+    parser.add_argument('--h_min', type=int, default=0)
+    parser.add_argument('--h_max', type=int, default=360)
+    parser.add_argument('--s_min', type=float, default=0)
+    parser.add_argument('--s_max', type=float, default=1)
+    parser.add_argument('--v_min', type=float, default=0)
+    parser.add_argument('--v_max', type=float, default=1)
+    parser.add_argument('--bbox_min_x', type=float, default=0)
+    parser.add_argument('--bbox_max_x', type=float, default=1)
+    parser.add_argument('--bbox_min_y', type=float, default=0)
+    parser.add_argument('--bbox_max_y', type=float, default=0)
+    parser.add_argument('--bbox_min_z', type=float, default=1)
+    parser.add_argument('--bbox_max_z', type=float, default=1)
+
+    args = parser.parse_args()
+
+
+    pcd = o3d.io.read_point_cloud(args.input_file)
+    points = np.array(pcd.points)
+    colors = np.array(pcd.colors)
+    cur_pcd = pcd
+
+    hsv_min = [args.h_min, args.s_min, args.v_min]
+    hsv_max = [args.h_max, args.s_max, args.v_max]
+    print(len(points))
+
+    for ch in range(3):
+        cur_colors = np.array(cur_pcd.colors)
+        hsv_colors = rgb2hsv(cur_colors.reshape((cur_colors.shape[0], -1, 3)))
+        hsv_colors = hsv_colors[:,0,:]
+        new_pcd = cur_pcd.select_by_index(np.where(np.logical_and(hsv_colors[:,ch]<hsv_max[ch], hsv_colors[:,ch]>hsv_min[ch]))[0])
+        cur_pcd = new_pcd
+        print(len(cur_pcd.points))
+
+    
+    np_points = np.array(cur_pcd.points)
+    print(np_points.shape)
+    min_bound_x = np.quantile(np_points[:,0], args.bbox_min_x, axis=0)
+    max_bound_x = np.quantile(np_points[:,0], args.bbox_max_x, axis=0)
+    min_bound_y = np.quantile(np_points[:,1], args.bbox_min_y, axis=0)
+    max_bound_y = np.quantile(np_points[:,1], args.bbox_max_y, axis=0)
+    min_bound_z = np.quantile(np_points[:,2], args.bbox_min_z, axis=0)
+    max_bound_z = np.quantile(np_points[:,2], args.bbox_max_z, axis=0)
+    print(min_bound_x, min_bound_y, min_bound_z, max_bound_x, max_bound_y, max_bound_z)
+    min_bound = np.array([min_bound_x, min_bound_y, min_bound_z])
+    max_bound = np.array([max_bound_x, max_bound_y, max_bound_z])
+
+    bbox = o3d.geometry.AxisAlignedBoundingBox(min_bound=min_bound, max_bound=max_bound)
+    cur_pcd = cur_pcd.crop(bbox)
+    print(len(cur_pcd.points))
+
+
+    o3d.io.write_point_cloud(args.output_file, cur_pcd)
+    o3d.visualization.draw_geometries([cur_pcd])
+
+''''
 hsv = rgb2hsv(colors.reshape((colors.shape[0], -1, 3)))
 hsv = hsv[:,0,:]
 
@@ -46,20 +104,10 @@ new_pcd = pcd.select_by_index(np.where(np.logical_or(hsv[:,0]<20, hsv[:,0]>320))
 #o3d.io.write_point_cloud(f'colmap_exports/0421/0421_recons/{data}/phone_24mm/colmap_00-1/dense/fused_filtered.ply', new_pcd)
 o3d.io.write_point_cloud(f'{pcd_name}_filtered.ply', new_pcd)
 o3d.visualization.draw_geometries([new_pcd])
+'''
 
 '''
 
-min_bound = np.quantile(np_points, .01, axis=0)
-max_bound = np.quantile(np_points, .99, axis=0)
-
-bbox = o3d.geometry.AxisAlignedBoundingBox(min_bound=min_bound, max_bound=max_bound)
-crop_pcd = pcd.crop(bbox)
-
-o3d.io.write_point_cloud('colmap_exports/alpha_50mm_dense_crop.ply', crop_pcd)
-print(pcd.get_min_bound(), pcd.get_max_bound())
-print(crop_pcd.get_min_bound(), crop_pcd.get_max_bound())
-
-print(crop_pcd.colors)
 '''
 
 #o3d.visualization.draw_geometries([crop_pcd])
